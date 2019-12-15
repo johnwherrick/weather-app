0a4ce852c7d2a526ba0d40c491da9b5d67774532
John W. Herrick <johnwherrick@gmail.com>
2019-12-15 09:10:52 -0600

    Add CLI for location

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Destin', (err, res) => {
-    if (err) {
-        return console.log(err)
-    }
-    forecast(res.lat, res.lon, (error, forecast) => {
+// Variable to hold location to get weather for
+const location = process.argv[2]
+
+if (location) {
+    geocode(location, (err, res) => {
         if (err) {
             return console.log(err)
         }
-        console.log(res.loc)
-        console.log(forecast)
+        forecast(res.lat, res.lon, (error, forecast) => {
+            if (err) {
+                return console.log(err)
+            }
+            console.log(res.loc)
+            console.log(forecast)
+        })
     })
-})
-
+} else {
+    console.log('You must enter a location. Try again!')
+}
